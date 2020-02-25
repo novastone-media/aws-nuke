d1b08e9f91d00424d6c7d3abcfe88a8e2653b88d
Joe Chung <joseph.chung@novastonemedia.com>
2020-02-25 13:34:31 +0000

    NS-2448: Added resource for CloudFront OAI

---
+++ resources/cloudfront-origin-access-identities.go
@@ -0,0 +1,55 @@
+package resources
+
+import (
+    "github.com/aws/aws-sdk-go/aws/session"
+    "github.com/aws/aws-sdk-go/service/cloudfront"
+)
+
+type CloudFrontOriginAccessIdentity struct {
+    svc *cloudfront.CloudFront
+    ID  *string
+}
+
+func init() {
+    register("CloudFrontOriginAccessIdentity", ListCloudFrontOriginAccessIdentities)
+}
+
+func ListCloudFrontOriginAccessIdentities(sess *session.Session) ([]Resource, error) {
+  svc := cloudfront.New(sess)
+  resources := []Resource{}
+
+  for {
+    resp, err := svc.ListCloudFrontOriginAccessIdentities(nil)
+    if err != nil {
+        return nil, err
+    }
+
+    for _, identity := range resp.CloudFrontOriginAccessIdentityList.Items {
+      resources = append(resources,&CloudFrontOriginAccessIdentity{
+        svc: svc,
+        ID:  item.Id,
+      })
+    }
+    return resources, nil
+  }
+}
+
+func (f *CloudFrontOriginAccessIdentity) Remove() error {
+  resp, err := f.svc.GetCloudFrontOriginAccessIdentity(&cloudfront.GetCloudFrontOriginAccessIdentityInput{
+    Id: f.ID,
+  })
+  if err != nil {
+    return err
+  }
+
+  _, err = f.svc.DeleteCloudFrontOriginAccessIdentity(&cloudfront.DeleteCloudFrontOriginAccessIdentityInput{
+    Id: f.ID,
+    IfMatch: resp.ETag,
+  })
+
+  return err
+}
+
+func (f *CloudFrontOriginAccessIdentity) String() string {
+  return *f.ID
+}
